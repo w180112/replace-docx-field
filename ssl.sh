4272edcab85787fb041adf64edec31086bee0ead
the <w180112@gmail.com>
2023-04-29 23:22:35 +0800

    modify ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,3 +1,9 @@
 set -ex
 openssl req -newkey rsa:4096 -nodes -keyout server.key -out server.csr
 openssl x509 -signkey server.key -in server.csr -req -days 365 -out server.crt
+certs_dir_count=$(ls -l | grep certs | wc -l)
+if [ $certs_dir_count -eq 0 ]; then
+	mkdir certs
+fi
+mv server.crt certs/
+mv server.key certs/
